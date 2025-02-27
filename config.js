e4878be7a325fb3c6f9283b3846cf345eca2ffc0
Damien Rozan <damien.rozan@gmail.com>
2025-02-27 13:38:19 +0200

    Final commit - ensuring all files are included

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 // eslint-disable-next-line no-undef
 module.exports = {
-    API_URL: "https://cnt-4ac6c647-c224-492a-a3ac-e856d6ab156b.containerhub.tripleten-services.com/" // no trailing slash
+    API_URL: "https://cnt-b03b2f36-6eca-4872-8a2d-b9d816b9b8b8.containerhub.tripleten-services.com/" // no trailing slash
 }
