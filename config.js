7c09ac7ab1fe6bbbb97851dd4e13eb75d30f16da
Damien Rozan <damien.rozan@gmail.com>
2025-02-27 13:53:37 +0200

    Updated API URL in config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 // eslint-disable-next-line no-undef
 module.exports = {
-    API_URL: "https://cnt-b03b2f36-6eca-4872-8a2d-b9d816b9b8b8.containerhub.tripleten-services.com/" // no trailing slash
+    API_URL: "https://cnt-68dddfdb-a93e-443f-b7fc-7c5d86e304ac.containerhub.tripleten-services.com/" // no trailing slash
 }
